073b63aef6312a7494f617219cd25315dbac6f89
Ruben Alvarado <ruben.alvardo@plexusworldwide.com>
2023-09-06 14:32:51 -0600

    added strict validations

--- src/shared/models/dtos/user/updatewebuser.dto.ts
+++ src/shared/models/dtos/user/updatewebuser.dto.ts
@@ -1,4 +1,67 @@
-import { PartialType } from '@nestjs/swagger';
-import { WebUserDTO } from './createwebuser.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsMobilePhone,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  IsUrl,
+  MaxLength,
+} from 'class-validator';
+export class UpdateWebUserDTO {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(150)
+  name?: string;
 
-export class UpdateWebUserDTO extends PartialType(WebUserDTO) {}
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(150)
+  lastName?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @MaxLength(150)
+  secondLast?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsMobilePhone()
+  phone?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsMongoId()
+  role?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(150)
+  email?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsPhoneNumber()
+  phoneNumber?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(150)
+  displayName?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsUrl()
+  photoUrl?: string;
+}
