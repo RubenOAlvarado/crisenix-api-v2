b282cd931cce153a4fad70cf2b520403b6cc3659
Ruben Alvarado <ruben.alvardo@plexusworldwide.com>
2023-11-29 14:26:50 -0600

    chore: added new endpoints to destinations module

--- src/destination/destination.controller.ts
+++ src/destination/destination.controller.ts
@@ -1,9 +1,20 @@
 import { PaginatedDTO } from '@/shared/dtos/paginated.dto';
 import { ResponseDestinationDTO } from '@/shared/models/dtos/destination/responsedestination.dto';
 import { UrlValidator } from '@/shared/validators/urlValidator.dto';
-import { Controller, Get, Param, Body, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Body,
+  Post,
+  Query,
+  Put,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 import {
   ApiBody,
+  ApiCreatedResponse,
   ApiExtraModels,
   ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
@@ -14,12 +25,46 @@ import {
 import { DestinationService } from './destination.service';
 import { Public } from '@/auth/public.decorator';
 import { SearcherDTO } from '@/shared/dtos/searcher.dto';
+import { CreateDestinationDTO } from '@/shared/models/dtos/destination/createdestination.dto';
+import { ApiPaginatedResponse } from '@/shared/decorators/api-paginated.response.dto';
+import { QueryDTO } from '@/shared/dtos/query.dto';
+import { PaginateResult } from '@/shared/interfaces/paginate.interface';
+import { UpdateDestinationDTO } from '@/shared/models/dtos/destination/updatedestination.dto';
 
 @Controller('destination')
 @ApiTags('Destination')
 @ApiExtraModels(PaginatedDTO)
 export class DestinationController {
   constructor(private readonly destinationService: DestinationService) {}
+
+  @ApiCreatedResponse({
+    description: 'The destination has been created.',
+    type: ResponseDestinationDTO,
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Something went wrong creating the destination.',
+  })
+  @ApiBody({ type: CreateDestinationDTO })
+  @Post('/create')
+  async create(
+    @Body() createDestinationDTO: CreateDestinationDTO,
+  ): Promise<ResponseDestinationDTO> {
+    return await this.destinationService.create(createDestinationDTO);
+  }
+
+  @ApiPaginatedResponse(ResponseDestinationDTO)
+  @ApiInternalServerErrorResponse({
+    description: 'Something went wrong finding the destinations.',
+  })
+  @ApiNotFoundResponse({
+    description: 'Destinations not found.',
+  })
+  @Get()
+  async findAll(
+    @Query() queryDTO: QueryDTO,
+  ): Promise<PaginateResult<ResponseDestinationDTO>> {
+    return await this.destinationService.findAll(queryDTO);
+  }
 
   @ApiOkResponse({
     description: 'The destination has been found.',
@@ -37,6 +82,75 @@ export class DestinationController {
     @Param() urlValidator: UrlValidator,
   ): Promise<ResponseDestinationDTO> {
     return await this.destinationService.findOne(urlValidator);
+  }
+
+  @ApiOkResponse({
+    description: 'The destination have been found.',
+    type: ResponseDestinationDTO,
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Something went wrong finding the destination.',
+  })
+  @ApiNotFoundResponse({
+    description: 'Destination not found.',
+  })
+  @Get('web/:id')
+  async findOneWeb(
+    @Param() urlValidator: UrlValidator,
+  ): Promise<ResponseDestinationDTO> {
+    return await this.destinationService.findOneWeb(urlValidator);
+  }
+
+  @ApiOkResponse({
+    description: 'Destination successfully updated.',
+    type: ResponseDestinationDTO,
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Something went wrong updating the destination.',
+  })
+  @ApiNotFoundResponse({
+    description: 'Destination not found.',
+  })
+  @ApiBody({ type: UpdateDestinationDTO })
+  @Put('/:id')
+  async update(
+    @Param() urlValidator: UrlValidator,
+    @Body() updateDestinationDTO: UpdateDestinationDTO,
+  ): Promise<ResponseDestinationDTO> {
+    return await this.destinationService.update(
+      urlValidator,
+      updateDestinationDTO,
+    );
+  }
+
+  @ApiOkResponse({
+    description: 'Destination successfully deleted.',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Something went wrong deleting the destination.',
+  })
+  @ApiNotFoundResponse({
+    description: 'Destination not found.',
+  })
+  @Delete('/:id')
+  async delete(@Param() urlValidator: UrlValidator): Promise<string> {
+    await this.destinationService.delete(urlValidator);
+    return 'Destination successfully deleted.';
+  }
+
+  @ApiOkResponse({
+    description: 'Destination successfully reactivated.',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Something went wrong reactivating the destination.',
+  })
+  @ApiNotFoundResponse({
+    description: 'Destination not found.',
+  })
+  @Patch('reactivate/:id')
+  async reactivate(@Param() urlValidator: UrlValidator): Promise<string> {
+    await this.destinationService.reactivate(urlValidator);
+    return 'Destination successfully reactivated.';
   }
 
   // TODO: improve this endpoint docs
--- src/destination/destination.service.ts
+++ src/destination/destination.service.ts
@@ -1,5 +1,10 @@
+import { QueryDTO } from '@/shared/dtos/query.dto';
 import { SearcherDTO } from '@/shared/dtos/searcher.dto';
+import { Status } from '@/shared/enums/status.enum';
 import { DestinationLean } from '@/shared/interfaces/destination/destination.interface';
+import { PaginateResult } from '@/shared/interfaces/paginate.interface';
+import { CreateDestinationDTO } from '@/shared/models/dtos/destination/createdestination.dto';
+import { UpdateDestinationDTO } from '@/shared/models/dtos/destination/updatedestination.dto';
 import { Destinations } from '@/shared/models/schemas/destination.schema';
 import { UrlValidator } from '@/shared/validators/urlValidator.dto';
 import {
@@ -20,6 +25,76 @@ export class DestinationService {
 
   private readonly logger = new Logger(DestinationService.name);
 
+  async create(
+    createDestinationDTO: CreateDestinationDTO,
+  ): Promise<DestinationLean> {
+    try {
+      this.logger.debug('creating destination');
+      const destination = await new this.destinationModel(
+        createDestinationDTO,
+      ).save();
+      return destination.toObject();
+    } catch (error) {
+      this.logger.error(
+        `Something went wrong while creating destination: ${error}`,
+      );
+      throw new InternalServerErrorException(
+        'Something went wrong while creating destination.',
+      );
+    }
+  }
+
+  async findAll({
+    page,
+    limit,
+    status,
+  }: QueryDTO): Promise<PaginateResult<DestinationLean>> {
+    try {
+      this.logger.debug(`finding destinations with status: ${status}`);
+      const docs = status
+        ? await this.destinationModel
+            .find({ status })
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .populate('originCity')
+            .populate('category')
+            .populate('translationType')
+            .populate('aboardPoint')
+            .select({ __v: 0, createdAt: 0 })
+            .lean()
+        : await this.destinationModel
+            .find()
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .populate('originCity')
+            .populate('category')
+            .populate('translationType')
+            .populate('aboardPoint')
+            .select({ __v: 0, createdAt: 0 })
+            .lean();
+      if (!docs.length) throw new NotFoundException('Destinations not found.');
+      const totalDocs = status
+        ? await this.destinationModel.countDocuments({ status })
+        : await this.destinationModel.countDocuments();
+      return {
+        docs,
+        totalDocs,
+        hasPrevPage: page > 1,
+        hasNextPage: page < Math.ceil(totalDocs / limit),
+        page,
+        totalPages: Math.ceil(totalDocs / limit),
+      };
+    } catch (error) {
+      this.logger.error(
+        `Something went wrong while finding destinations: ${error}`,
+      );
+      if (error instanceof NotFoundException) throw error;
+      throw new InternalServerErrorException(
+        'Something went wrong while finding destinations.',
+      );
+    }
+  }
+
   async findOne({ id }: UrlValidator): Promise<DestinationLean> {
     try {
       this.logger.debug(`finding destination with id: ${id}`);
@@ -28,6 +103,7 @@ export class DestinationService {
         .populate('originCity')
         .populate('category')
         .populate('translationType')
+        .populate('aboardPoint')
         .select({ __v: 0, createdAt: 0 })
         .lean();
       if (!destination) throw new NotFoundException('Destination not found.');
@@ -39,6 +115,90 @@ export class DestinationService {
       if (error instanceof NotFoundException) throw error;
       throw new InternalServerErrorException(
         'Something went wrong while finding destination.',
+      );
+    }
+  }
+
+  async findOneWeb({ id }: UrlValidator): Promise<DestinationLean> {
+    try {
+      this.logger.debug(`finding destination with id: ${id}`);
+      const destination = await this.destinationModel
+        .findById(id)
+        .select({ __v: 0, createdAt: 0 })
+        .lean();
+      if (!destination) throw new NotFoundException('Destination not found.');
+      return destination;
+    } catch (error) {
+      this.logger.error(
+        `Something went wrong while finding destination: ${error}`,
+      );
+      if (error instanceof NotFoundException) throw error;
+      throw new InternalServerErrorException(
+        'Something went wrong while finding destination.',
+      );
+    }
+  }
+
+  async update(
+    { id }: UrlValidator,
+    updateDestinationDTO: UpdateDestinationDTO,
+  ): Promise<DestinationLean> {
+    try {
+      this.logger.debug(`updating destination with id: ${id}`);
+      const destination = await this.destinationModel
+        .findByIdAndUpdate(id, updateDestinationDTO, { new: true })
+        .select({ __v: 0, createdAt: 0 })
+        .lean();
+
+      if (!destination) throw new NotFoundException('Destination not found.');
+      return destination;
+    } catch (error) {
+      this.logger.error(
+        `Something went wrong while updating destination: ${error}`,
+      );
+      if (error instanceof NotFoundException) throw error;
+      throw new InternalServerErrorException(
+        'Something went wrong while updating destination.',
+      );
+    }
+  }
+
+  async delete({ id }: UrlValidator): Promise<void> {
+    try {
+      this.logger.debug(`deleting destination with id: ${id}`);
+      const destination = await this.destinationModel.findByIdAndUpdate(
+        id,
+        { status: Status.INACTIVE },
+        { new: true },
+      );
+      if (!destination) throw new NotFoundException('Destination not found.');
+    } catch (error) {
+      this.logger.error(
+        `Something went wrong while deleting destination: ${error}`,
+      );
+      if (error instanceof NotFoundException) throw error;
+      throw new InternalServerErrorException(
+        'Something went wrong while deleting destination.',
+      );
+    }
+  }
+
+  async reactivate({ id }: UrlValidator): Promise<void> {
+    try {
+      this.logger.debug(`reactivating destination with id: ${id}`);
+      const destination = await this.destinationModel.findByIdAndUpdate(
+        id,
+        { status: Status.ACTIVE },
+        { new: true },
+      );
+      if (!destination) throw new NotFoundException('Destination not found.');
+    } catch (error) {
+      this.logger.error(
+        `Something went wrong while reactivating destination: ${error}`,
+      );
+      if (error instanceof NotFoundException) throw error;
+      throw new InternalServerErrorException(
+        'Something went wrong while reactivating destination.',
       );
     }
   }
