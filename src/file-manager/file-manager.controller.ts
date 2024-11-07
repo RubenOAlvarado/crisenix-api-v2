5f09c2885ca6142df4955780dddd037f98b6b3ff
Ruben Alvarado <ruben.alvardo@plexusworldwide.com>
2024-11-06 19:35:16 -0600

    fix: build

--- src/file-manager/file-manager.controller.ts
+++ src/file-manager/file-manager.controller.ts
@@ -4,7 +4,6 @@ import {
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileManagerService } from './file-manager.service';
 import { ApiExcludeController, ApiExcludeEndpoint } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { excelFileFilter } from '@/shared/utilities/fileManagement.utils';
@@ -14,28 +13,11 @@ import { CatalogManagerService } from './catalog-manager.service';
 @ApiExcludeController()
 @Controller('file-manager')
 export class FileManagerController {
-  constructor(
-    private readonly fileManagerService: FileManagerService,
-    private readonly catalogFileManager: CatalogManagerService,
-  ) {}
+  constructor(private readonly catalogFileManager: CatalogManagerService) {}
 
   @ApiExcludeEndpoint()
   @Public()
   @Post()
-  @UseInterceptors(
-    FileInterceptor('file', {
-      dest: './uploads',
-      fileFilter: excelFileFilter,
-    }),
-  )
-  async load(@UploadedFile() file: Express.Multer.File) {
-    // deepcode ignore PT: <please specify a reason of ignoring this>
-    await this.fileManagerService.loadToursAndDestinations(file.path);
-  }
-
-  @ApiExcludeEndpoint()
-  @Public()
-  @Post('catalogs')
   @UseInterceptors(
     FileInterceptor('file', {
       dest: './uploads',
